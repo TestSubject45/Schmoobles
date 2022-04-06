66cb15af136e6ae94d71b707d53ef97b962f6c02
TestSubject45 <blakegambel@yahoo.com>
2022-04-05 18:32:47 -0600

    Added ID to Schmooble class
    Also removed debugging statements

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random
 import turtle
 from schmooble import Schmooble, foodList, mapRange
 
-startPop = 5
+startPop = 10
 
 myscreen = turtle.Screen()
 
@@ -14,21 +14,21 @@ myscreen.title('Turtle Game')
 turtleList = []
 schmoobleList = []
 for i in range(0,startPop):
-	schmoobleList.append(Schmooble())
+	schmoobleList.append(Schmooble(i))
 
 
 running = True
 while running:
 	deadPop = 0
 	for i in range(0,startPop):
-		print("\n\nSchmooble #",i)
+#		print("\n\nSchmooble #",i)
 		schmoobleList[i].update()
 		if schmoobleList[i].state == 4:
 			deadPop = deadPop + 1
-	print("len of schmoobleList",len(schmoobleList))
+#	print("len of schmoobleList",len(schmoobleList))
 	if deadPop == len(schmoobleList):
 		running = False
-	print("Total dead population:",deadPop)		
+#	print("Total dead population:",deadPop)		
 
 input("All population has died...")
 
--- schmooble.py
+++ schmooble.py
@@ -1,8 +1,8 @@
 import random
 import turtle
 
-foodDistanceLimit = 300
-foodLimit = 10
+foodDistanceLimit = 500
+foodLimit = 20
 
 class FoodList:
 	def __init__(self,foodDistanceLimit,foodLimit):
@@ -42,7 +42,7 @@ foodList = FoodList(foodDistanceLimit,foodLimit)
 
 
 class Schmooble:
-	def __init__(self,parent1=None,parent2=None):
+	def __init__(self,idnum,parent1=None,parent2=None):
 		if parent1 == None or parent2 == None:
 			self.speed = random.randint(1,10)
 			self.energy = random.randint(5,200)
@@ -59,17 +59,17 @@ class Schmooble:
 		self.state = 0 #States: 0 = search, 1 = move, 2 = wander, 3 = mate, 4 = dead
 		self.destination = self.turtle.pos()
 		self.timeSpentWandering = 0
-		print("Mating Threshold",self.matingEnergyThreshold)
+		self.id = idnum
 
 	def spendEnergy(self,energy):
 		self.energy = self.energy - energy
 		if self.energy <= 0:
 			self.starve()
-		print("Energy Remaining:",self.energy)
+#		print("Energy Remaining:",self.energy)
 
 	def starve(self):
-		print("No energy!")
-		print("This Schmooble has died.")
+#		print("No energy!")
+#		print("This Schmooble has died.")
 		self.state = 4
 
 	def setSpeed(self,speed):
@@ -81,7 +81,7 @@ class Schmooble:
 			self.turtle.speed = speed
 
 	def update(self):
-		print("State:",self.state)
+#		print("State:",self.state)
 		global foodList
 		if self.state == 0: #Search for Food
 			self.destination = self.searchForFood(foodList.list)
@@ -95,14 +95,14 @@ class Schmooble:
 			pass
 
 	def searchForFood(self,foodList):
-		print("Searching for food...")
+#		print("Searching for food...")
 		closestPoint = (-500000,-500000)
 		for foodSpot in foodList:
 			if self.turtle.distance(foodSpot) <= self.sightRange:
-				print("Found food in range! at",foodSpot)
+#				print("Found food in range! at",foodSpot)
 				if self.turtle.distance(foodSpot) < self.turtle.distance(closestPoint):
 					closestPoint = foodSpot
-					print("Closest point of food:",closestPoint)
+#					print("Closest point of food:",closestPoint)
 		self.state = 1
 		if closestPoint == (-500000,-500000):
 			self.state = 2
@@ -123,26 +123,27 @@ class Schmooble:
 		self.turtle.setheading(self.turtle.towards(point))
 		self.turtle.forward(self.speed)
 		if self.foodAtPoint(point) == False:
-			print("Food at point has dissapeared!")
+#			print("Food at point has dissapeared!")
 			self.state = 0
 		if self.turtle.distance(point) < 5:
 			if self.foodAtPoint(point):
-				print("Yum!")
+#				print("Yum!")
 				self.eat(50)
 				foodList.removePoint(point)
 				self.state = 0
 			else:
-				print("No food here now!")
+#				print("No food here now!")
 				self.state = 0
 
 	def eat(self,energy):
 		self.energy = self.energy + energy
-		print("Current Energy:",self.energy)
+#		print("Current Energy:",self.energy)
 		if self.energy >= self.matingEnergyThreshold:
 			self.state = 3
 
 	def searchForMate(self):
 		print("#######################Energy threshold crossed, searching for mate!")
+		self.creatureInfo()
 		pass
 
 	def wander(self):
@@ -176,6 +177,7 @@ class Schmooble:
 		self.mutationRate = round(mutationRate,2)
 
 	def creatureInfo(self):
+		print("ID Number:",self.id)
 		print("Speed:",self.speed)
 		print("Energy:",self.energy)
 		print("Mating Threshold:",self.matingEnergyThreshold)
