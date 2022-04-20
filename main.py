4c4deb649d975a20bb3515c3f61992bd89d6826f
TestSubject45 <blakegambel@yahoo.com>
2022-04-20 17:56:47 -0600

    Script stops when all pops are dead

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 import random
 import turtle
 
-myscreen = turtle.Screen()
+from helpers import findMidpoint, mapRange, randomPosition, getDeadPopulation, getLivingPopulation
 
+myscreen = turtle.Screen()
 myscreen.bgcolor('light green')
 myscreen.setup(0.8,0.8)
 myscreen.title('Schmoobles')
 
 from foodlist import foodList
-
 from schmooble import Schmooble
 
-firstSchmooble = Schmooble(1,(-5,15))
+def mainLoop(initialPopulation):
+	population = {}
+	while len(population) < initialPopulation:
+		population[len(population)+1] = Schmooble(len(population)+1,randomPosition(100))
 
-tmp = input("...")
+	running = True
+	while running:
+		deadPop = getDeadPopulation(population)
+		livingPop = getLivingPopulation(population)
+		if len(deadPop) == len(population):
+			print("Everyone's dead!")
+			running = False
+		else:
+			for schmoobleID in livingPop:
+				population[schmoobleID].tick()
 
+
+
+
+mainLoop(3)
 turtle.done()
